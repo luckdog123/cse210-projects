0d50debf615a3adab6b8066dbe4b4dfae3f01527
Spencer Greenhalgh <potofgold093@gmail.com>
2025-02-25 09:59:14 -0700

    Stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Spandys World!");
+        Console.WriteLine("why me?????????????????");
+        string name = Console.ReadLine();
+        Console.WriteLine($"This is your name {name}");
     }
 }
