ec67a238c2b293d85bcfc124376a7b3c4569f14a
Spencer Greenhalgh <potofgold093@gmail.com>
2025-02-26 09:37:16 -0700

    Tada

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("Now what is your last name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"This is your name, {first_name.ToUpper()} {last_name}.");
     }
 }
