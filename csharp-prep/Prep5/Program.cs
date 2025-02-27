a899dae543f1398f94820ca552a817d76acea7e2
Spencer Greenhalgh <potofgold093@gmail.com>
2025-02-27 13:24:17 -0700

    oOOps forgot to savwe the whole time but its done now :)

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static float SquareDaNum(float favoritNumber){
+            return favoritNumber * favoritNumber;   
+        }
+        static string getDaName(){
+            Console.WriteLine("Hello stranger! Welcome to the program!!");
+            Console.WriteLine("Now its time for us to get to know you what is your name?");
+            string name  = Console.ReadLine();
+            return name;
+        } 
+        static float getDaFavoriteNumber(){
+            Console.WriteLine("What is your favortie number?");
+            float favNum = float.Parse(Console.ReadLine());  
+            return SquareDaNum(favNum);                      
+        }
+        static void finalState(string nam , float favnumnum){
+            Console.WriteLine($"{nam} your favorite number squared is {favnumnum}.");
+        }
+        static void daFunction(){
+            string nam = getDaName();
+            float favnumnum = getDaFavoriteNumber();
+            finalState(nam , favnumnum);
+        }
+        daFunction();
     }
 }
