ab6b1cc294fbeb0d5d0c2243cc4af3a1e3bbcb04
Spencer Greenhalgh <potofgold093@gmail.com>
2025-02-26 17:28:40 -0700

    it has beguuung

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string hint = "No hint yet";
+        string go_again = "YES";
+        do{
+            
+            Random randomGenerator = new Random();
+            int magic_number = randomGenerator.Next(1, 67);
+            Console.WriteLine("Welcome to the Magic number game guess a number between 1 and 67. A whole number.");
+
+            Console.WriteLine($"Now guess again you guess needs to be {hint}");
+        } while (go_again == "YES");
     }
 }
