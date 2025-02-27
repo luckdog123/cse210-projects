13250ae556c7a2d9c25fb7c3c976c1229acc21d1
Spencer Greenhalgh <potofgold093@gmail.com>
2025-02-26 19:05:54 -0700

    once again i am here

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> lystsTM = [];
+        string user_input = "Go";
+
+        Console.WriteLine("Hello welcome to the lystsTM. Please input the numbers you disire. Hit enter after each entry, and when you are done type 'DONE'.");
+        do{
+            user_input = Console.ReadLine();
+            if (user_input != "DONE"){
+                lystsTM.Add(int.Parse(user_input));
+            }
+        } while (user_input != "DONE");
+        foreach (int i in lystsTM){
+            Console.Write(lystsTM);
+        }
+        
     }
 }
