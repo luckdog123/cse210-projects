5bdd8cc1c4f244ef925d2f6ca6c8304cf602cf4b
Spencer Greenhalgh <potofgold093@gmail.com>
2025-03-06 10:43:30 -0700

    menu is up and running

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string menu_input = "five";
+        while (menu_input != "5"){
+            
+            Console.WriteLine("Stuff");
+            menu_input = Console.ReadLine();
+            if (menu_input == "1"){
+                Console.WriteLine("why mee");
+            }
+            else if (menu_input == "2"){
+                Console.WriteLine("why mee");
+            }
+            else if (menu_input == "3"){
+                Console.WriteLine("why mee");
+            }
+            else if (menu_input == "4"){
+                Console.WriteLine("why mee");
+            }
+            else if (menu_input == "5"){
+                Console.WriteLine("why mee");
+            }
+            
+
+
+        }
+        Console.WriteLine("why mee");
     }
 }
