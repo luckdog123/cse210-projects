c59340fb4d35b51c1bf9a35f3ac63fe399ee34d8
Spencer Greenhalgh <potofgold093@gmail.com>
2025-03-19 01:26:10 -0600

    made good progress

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        List<string> frame = new List<string>();
+        frame.Add("10");
+        frame.Add("20");
+        frame.Add("30");
+        frame.Add("40");
+        
+        // AnimateConsole ac = new AnimateConsole(10, frame, 3000);
+        // ac.Animate();
+        AnimateConsole ac2 = new AnimateConsole(18, 500);
+        ac2.SetFramesForSpinny();
+        ac2.Animate();
     }
 }
