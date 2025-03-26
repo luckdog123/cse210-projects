e5e51df2814f42d13ad6abe1187743a6a239ce1c
Spencer Greenhalgh <potofgold093@gmail.com>
2025-03-26 13:28:34 -0600

    gg

--- prove/Develop05/menu.cs
+++ prove/Develop05/menu.cs
@@ -1,11 +1,17 @@
+
 class Menu
 {
     public static void menuStuffAndThings()
     {
-        int userinput = Console.Read();
+        int userinput = 33;
+
         while (userinput != 6)
         {
-            switch(userinput)
+            // Console.Clear();
+            Console.WriteLine("1.\n2.\n3.\n4.\n5.\n6. Quit :(");
+            userinput = int.Parse(Console.ReadLine());
+
+            switch (userinput)
             {
                 case 1:
                     Console.WriteLine("1");
@@ -22,8 +28,13 @@ class Menu
                 case 5:
                     Console.WriteLine("5");
                     break;
-                case 6:
-                    Console.WriteLine("Bye!!!!");
+                 case 6:
+                        Console.WriteLine("\nExiting the application. Goodbye!");
+                        break;
+                default:
+                    Console.WriteLine("\nInvalid input. Please enter a number between 1 and 6.");
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadLine();
                     break;
             }
                 
