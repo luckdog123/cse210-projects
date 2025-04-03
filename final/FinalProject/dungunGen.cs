b78ea439be9b49162c825f19d6fa0174a0a55a4c
Spencer Greenhalgh <potofgold093@gmail.com>
2025-04-02 20:10:20 -0600

    starting finally

--- final/FinalProject/dungunGen.cs
+++ final/FinalProject/dungunGen.cs
@@ -1,4 +1,18 @@
 class DungenGen 
 {
-    public List<int> 
+    // public List<int> 
+    private int _dunLength = 10;
+    
+    public static int RandomizeDunLength()
+    {
+        int[] myArray = { 8,9,10,10,10,12,13,15,17};
+        // Create a Random object
+        Random random = new Random();
+        // Generate a random index within the bounds of the array
+        int randomIndex = random.Next(myArray.Length);
+        // Access the element at the random index
+        return myArray[randomIndex];
+    }
+
+    
 }
---
+++ final/FinalProject/mainGameloop.cs
@@ -0,0 +1,26 @@
+class GameLoop
+{
+
+    public static void MainGameLoop()
+    {
+        int roomNumber = 0;
+        int dunLength = DungenGen.RandomizeDunLength();
+        List<int> roomType = new List<int>(); 
+        // This is what drives the whole game
+        while(roomNumber <= dunLength)
+        {
+            if(roomNumber == 0){
+                // do the startin room else do the normal thing
+            }
+            else if (roomNumber == dunLength-1){
+                // boss fight time baby
+            }
+            else
+            {
+                // the normal part of the game
+                if(roomType[roomType.Count - 1] == )
+            }
+            
+        }
+    } 
+}
