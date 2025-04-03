816c8efbe363da6cf60854b6ac32a851c894047f
Spencer Greenhalgh <potofgold093@gmail.com>
2025-04-03 10:00:51 -0600

    gg

--- final/FinalProject/text prompt storage.cs
+++ final/FinalProject/text prompt storage.cs
@@ -2,31 +2,31 @@ class Prompt
 {
     // this is where all the prompts and the logic for getting the prompts will be housed
 
-    private string[] _toRoom1 = 
+    private static string[] _toRoom1 = 
     {
         "1this is a hint that the next room might be empty",
         "1this is the second",
         "1and the third"
     };
-    private string[] _toRoom2 = 
+    private static string[] _toRoom2 = 
     {
         "2this is a hint that the next room might be empty",
         "2this is the second",
         "2and the third"
     };
-    private string[] _toRoom3 = 
+    private static string[] _toRoom3 = 
     {
         "3this is a hint that the next room might be empty",
         "3this is the second",
         "3and the third"
     };
-    private string[] _toRoom4 = 
+    private static string[] _toRoom4 = 
     {
         "4this is a hint that the next room might be empty",
         "4this is the second",
         "4and the third"
     };
-    private string[] _toRoom5 = 
+    private static string[] _toRoom5 = 
     {
         "5this is a hint that the next room might be empty",
         "5this is the second",
@@ -34,12 +34,23 @@ class Prompt
     };
     public static string RoomHints(int NextPossibleRoom)
     {
-
-
-
-
-
-
-        return "";
+        
+        switch(NextPossibleRoom)
+        {
+            case 1:
+                Random random = new Random();
+                int promtChoice = random.Next(1, _toRoom1.Length);
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+        return roomHint;
     }
 }
